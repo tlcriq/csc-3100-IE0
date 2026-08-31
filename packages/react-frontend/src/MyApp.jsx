ba4fb4d32ca73a8bd3eead100a23aeea33324b5e
tlcriq <tatecriqui@gmail.com>
2026-08-31 16:33:21 -0700

    midway point

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -21,11 +21,17 @@ function MyApp() {
         job: "Bartender",
         },
   ]);
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
   return (
   <div className="container">
-    <Table characterData={characters} />
+    <Table characterData={characters} removeCharacter={removeOneCharacter} />
   </div>
-);
+  );
 }
 
 export default MyApp;
--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -17,6 +17,9 @@ function TableBody(props) {
       <tr key={index}>
         <td>{row.name}</td>
         <td>{row.job}</td>
+        <td>
+            <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
       </tr>
     );
    }
@@ -32,7 +35,10 @@ function Table(props) {
     return (
       <table>
         <TableHeader />
-        <TableBody characterData={props.characterData}/>
+        <TableBody
+            characterData={props.characterData}
+            removeCharacter={props.removeCharacter}
+        />
       </table>
     );
 }
