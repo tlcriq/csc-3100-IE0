f78a3ca544c3f3603cde5a249b375a501c23650f
tlcriq <tatecriqui@gmail.com>
2026-08-31 16:23:38 -0700

    Working on data

---
+++ packages/react-frontend/src/MyApp.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import Table from "./Table";
+
+
+function MyApp() {
+  const [characters, setCharacters] = useState([
+        {
+        name: "Charlie",
+        job: "Janitor",
+        },
+        {
+        name: "Mac",
+        job: "Bouncer",
+        },
+        {
+        name: "Dee",
+        job: "Aspring actress",
+        },
+        {
+        name: "Dennis",
+        job: "Bartender",
+        },
+  ]);
+  return (
+  <div className="container">
+    <Table characterData={characters} />
+  </div>
+);
+}
+
+export default MyApp;
